844b57d897ca5473147e6a0119966cf84acb0cf0
Dairo Quintero Camacho <dquinteroc@psl.com.co>
2021-05-19 07:39:31 -0500

    save response and get response with filter

--- src/test/java/co/com/api/test/dto/GithubDto.java
+++ src/test/java/co/com/api/test/dto/GithubDto.java
@@ -1,20 +1,18 @@
 package co.com.api.test.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GithubDto {
-  String id;
-  String name;
-  String full_Name;
-  String description;
-  String svn_url;
-  String default_Branch;
+  private String id;
+  private String name;
+  private String full_Name;
+  private String description;
+  private String svn_url;
+  private String default_Branch;
 
 }
